c1d3af47e05b564c0e4ecf680e4e56f576f26633
Adroaldo <hygino@msn.com>
2025-07-21 19:36:09 -0300

    Book database crud

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,11 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
-  constructor(private readonly bookService: BookService) {}
+  constructor(private readonly bookService: BookService) { }
 
   @Post()
   create(@Body() createBookDto: CreateBookDto) {
@@ -16,19 +15,19 @@ export class BookController {
   findAll() {
     return this.bookService.findAllBooks();
   }
-/*
+
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.bookService.findOne(+id);
+    return this.bookService.findBookById(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.bookService.update(+id, updateBookDto);
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateBookDto: CreateBookDto) {
+    return this.bookService.updateBook(+id, updateBookDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.bookService.remove(+id);
-  }*/
+    return this.bookService.removeBook(+id);
+  }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -8,14 +8,14 @@ import { ResponseBookDto } from './dto/response-book.dto';
 
 @Injectable()
 export class BookService {
-constructor(
+  constructor(
     @InjectRepository(Book)
     private readonly bookRepository: Repository<Book>,
     @InjectRepository(Author)
     private readonly authorRepository: Repository<Author>,
-  ) {}
+  ) { }
 
-async createBook(createBookDto: CreateBookDto): Promise<ResponseBookDto> {
+  async createBook(createBookDto: CreateBookDto): Promise<ResponseBookDto> {
     const author = await this.authorRepository.findOneBy({ id: createBookDto.authorId });
     if (!author) {
       throw new Error('Autor não encontrado');
@@ -26,12 +26,51 @@ async createBook(createBookDto: CreateBookDto): Promise<ResponseBookDto> {
     book.publisher = createBookDto.publisher;
     book.edition = createBookDto.edition;
     book.author = author;
-     const savedBook = await this.bookRepository.save(book);
-  return new ResponseBookDto(savedBook);
+    const savedBook = await this.bookRepository.save(book);
+
+    return new ResponseBookDto(savedBook);
   }
 
-async findAllBooks(): Promise<ResponseBookDto[]> {
+  async findAllBooks(): Promise<ResponseBookDto[]> {
     const books = await this.bookRepository.find({ relations: ['author'] });
     return books.map(book => new ResponseBookDto(book));
+  }
+
+  async findBookById(id: number): Promise<ResponseBookDto> {
+    const book = await this.bookRepository.findOne({ where: { id }, relations: ['author'] });
+    if (!book) {
+      throw new Error('Livro não encontrado');
+    }
+
+    return new ResponseBookDto(book);
+  }
+
+  async updateBook(id: number, updateBookDto: CreateBookDto): Promise<ResponseBookDto> {
+    const book = await this.bookRepository.findOne({ where: { id }, relations: ['author'] });
+    if (!book) {
+      throw new Error('Livro não encontrado');
+    }
+
+    const author = await this.authorRepository.findOneBy({ id: updateBookDto.authorId });
+    if (!author) {
+      throw new Error('Autor não encontrado');
+    }
+
+    book.title = updateBookDto.title;
+    book.publisher = updateBookDto.publisher;
+    book.edition = updateBookDto.edition;
+    book.author = author;
+    book.updatedAt = new Date();
+
+    const updatedBook = await this.bookRepository.save(book);
+    return new ResponseBookDto(updatedBook);
+  }
+
+  async removeBook(id: number): Promise<void> {
+    const book = await this.bookRepository.findOneBy({ id });
+    if (!book) {
+      throw new Error('Livro não encontrado');
+    }
+    await this.bookRepository.remove(book);
   }
 }
