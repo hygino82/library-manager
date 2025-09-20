23502c0f702b2c19133fde75100f3a07d877436f
Adroaldo <hygino@msn.com>
2025-09-19 22:19:18 -0300

    Fix application-test.properties

--- spring-backend/src/main/java/br/dev/hygino/LibraryManagerApplication.java
+++ spring-backend/src/main/java/br/dev/hygino/LibraryManagerApplication.java
@@ -2,8 +2,10 @@ package br.dev.hygino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class LibraryManagerApplication {
 
 	public static void main(String[] args) {
