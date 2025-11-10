4d166cbff2f933aadccfc64ee5e984e878f8d660
Adroaldo <hygino@msn.com>
2025-11-10 19:52:41 -0300

    Insert book loan on sql file

--- spring-backend/src/main/resources/import.sql
+++ spring-backend/src/main/resources/import.sql
@@ -2,26 +2,28 @@ INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_COD
 
 INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 3, 368, 2, 'José de Alencar', 'BR1002', 'Editora Ática', 'Iracema');
 
-INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (2, 2, 224, 3, 'Graciliano Ramos', 'BR1003', 'Editora Record', 'Vidas Secas');
+INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (3, 2, 224, 3, 'Graciliano Ramos', 'BR1003', 'Editora Record', 'Vidas Secas');
 
 INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 1, 208, 4, 'Clarice Lispector', 'BR1004', 'Rocco', 'A Hora da Estrela');
 
 INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 1, 160, 5, 'Manuel Antônio de Almeida', 'BR1005', 'Editora Saraiva', 'Memórias de um Sargento de Milícias');
 
-INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (2, 1, 336, 6, 'Jorge Amado', 'BR1006', 'Companhia das Letras', 'Capitães da Areia');
+INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 1, 336, 6, 'Jorge Amado', 'BR1006', 'Companhia das Letras', 'Capitães da Areia');
 
 INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 2, 320, 7, 'Euclides da Cunha', 'BR1007', 'Editora Cultrix', 'Os Sertões');
 
 INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 1, 288, 8, 'Raquel de Queiroz', 'BR1008', 'José Olympio', 'O Quinze');
 
-INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (2, 4, 180, 9, 'Aluísio Azevedo', 'BR1009', 'Martin Claret', 'O Cortiço');
+INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 4, 180, 9, 'Aluísio Azevedo', 'BR1009', 'Martin Claret', 'O Cortiço');
 
 INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 1, 272, 10, 'Lima Barreto', 'BR1010', 'Penguin Companhia', 'Triste Fim de Policarpo Quaresma');
 
-INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (2, 1, 247, 11, 'Machado de Assis', 'BR100125', 'Editora Record', 'Quincas Borba');
+INSERT INTO tb_book (BOOK_STATUS, EDITION, TOTAL_PAGES, ID, AUTHOR, PERSONAL_CODE, PUBLISHER, TITLE) VALUES (1, 1, 247, 11, 'Machado de Assis', 'BR100125', 'Editora Record', 'Quincas Borba');
 
-INSERT INTO tb_user (name, school_atribute, phone_number, email, has_loan) VALUES ('Juvenal Santos', 0, '7823739244', 'juvenal@email.com', false);
+INSERT INTO tb_user (name, school_atribute, phone_number, email, has_loan) VALUES ('Juvenal Santos', 0, '7823739244', 'juvenal@email.com', true);
 INSERT INTO tb_user (name, school_atribute, phone_number, email, has_loan) VALUES ('Maria Oliveira', 1, '7812345678', 'maria.oliveira@email.com', false);
 INSERT INTO tb_user (name, school_atribute, phone_number, email, has_loan) VALUES ('Carlos Souza', 2, '7898765432', 'carlos.souza@email.com', false);
 INSERT INTO tb_user (name, school_atribute, phone_number, email, has_loan) VALUES ('Ana Pereira', 1, '7823456789', 'ana.pereira@email.com', false);
 INSERT INTO tb_user (name, school_atribute, phone_number, email, has_loan) VALUES ('Rafael Lima', 0, '7876543210', 'rafael.lima@email.com', false);
+
+insert into tb_book_loan(end_date, start_date, book_id, user_id ) values ('2025-11-20',	'2025-11-10', 3, 1);
