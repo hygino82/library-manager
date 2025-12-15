3d3f96fe6f10d221fd41711f808dfaac14dd5eb8
Adroaldo <hygino@msn.com>
2025-12-15 18:38:46 -0300

    UserServiceTestIT: findUsersByName, findUser

--- spring-backend/src/test/java/br/dev/hygino/services/UserServiceTestIT.java
+++ spring-backend/src/test/java/br/dev/hygino/services/UserServiceTestIT.java
@@ -2,9 +2,9 @@ package br.dev.hygino.services;
 
 import br.dev.hygino.dto.ResponseUserDto;
 import br.dev.hygino.mappers.UserMapper;
-import br.dev.hygino.mappers.UserMapperImpl;
 import br.dev.hygino.models.SchoolAtribute;
 import br.dev.hygino.repositories.UserRepository;
+import br.dev.hygino.services.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,8 +14,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -29,6 +30,15 @@ public class UserServiceTestIT {
 
     @Autowired
     private UserService userService;
+
+    private UUID validId, dependentId, invalidId;
+
+    @BeforeEach
+    public void setup() {
+        validId = UUID.fromString("c7b2c61a-ff37-4a76-94ef-9c4d0b701005");
+        dependentId = UUID.fromString("c7b2c61a-ff37-4a76-94ef-9c4d0b701002");
+        invalidId = UUID.fromString("45ed9797-f0e5-424a-9a4c-e51952c91d71");
+    }
 
     @Test
     @DisplayName("FindUsersByName deve retornar uma página com um único elemento")
@@ -45,4 +55,24 @@ public class UserServiceTestIT {
         assertEquals(SchoolAtribute.SEXTO.name(), result.getContent().getFirst().schoolAtribute());
     }
 
+    @Test
+    @DisplayName("FindUser deve retornar um ResponseUserDto quando o id for válido")
+    public void findUserShouldReturnResponseUserDtoWhenValidId() {
+        final var result = userService.findUser(validId);
+
+        assertNotNull(result);
+        assertEquals("Rafael Lima", result.name());
+        assertEquals("rafael.lima@email.com", result.email());
+        assertEquals("7876543210", result.phoneNumber());
+        assertEquals(validId, result.id());
+        assertEquals(SchoolAtribute.SEXTO.name(), result.schoolAtribute());
+    }
+
+    @Test
+    @DisplayName("FindUser deve lançar UserNotFoundException quando o Id for inválido")
+    public void findUserShouldThrowUserNotFoundExceptionWhenInvalidId() {
+        final var result = assertThrows(UserNotFoundException.class, () -> userService.findUser(invalidId));
+
+        assertEquals("Não existe usuário com o Id: " + invalidId, result.getMessage());
+    }
 }
